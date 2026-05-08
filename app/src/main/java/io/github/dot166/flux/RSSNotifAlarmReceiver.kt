b665d661219386bdd0aa1b34e060e783cf111446
._______166 <retromac9x2@gmail.com>
2026-05-08 11:00:28 +0100

    fix AlarmReceiver

--- app/src/main/java/io/github/dot166/flux/RSSNotifAlarmReceiver.kt
+++ app/src/main/java/io/github/dot166/flux/RSSNotifAlarmReceiver.kt
@@ -5,7 +5,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import androidx.preference.PreferenceManager
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.future.future
@@ -14,19 +13,17 @@ class RSSNotifAlarmReceiver : BroadcastReceiver() {
     @OptIn(DelicateCoroutinesApi::class)
     override fun onReceive(context: Context?, intent: Intent?) {
         Log.i("RSS", "received")
-        if (context != null && PreferenceManager.getDefaultSharedPreferences(context)
-                .contains("RssUrls") && !PreferenceManager.getDefaultSharedPreferences(context)
-                .getString("RssUrls", "")!!
-                .isEmpty() && !PreferenceManager.getDefaultSharedPreferences(context)
-                .getString("RssUrls", "")!!.endsWith(";")
-        ) {
-            Log.i("RSS", "notif")
-            val notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val rssNotifier = RSSNotifier(notificationManager, context)
-            GlobalScope.future {
-                rssNotifier.showNotification()
-            }.get()
+        if (context != null) {
+            val repo = Repository.getInstance(context)
+            if (!repo.getFeeds().isEmpty()) {
+                Log.i("RSS", "notif")
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                val rssNotifier = RSSNotifier(notificationManager, context)
+                GlobalScope.future {
+                    rssNotifier.showNotification()
+                }.get()
+            }
         }
     }
 }
