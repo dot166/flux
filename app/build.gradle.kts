c622361be4e74c80d517fdf529683e2ce7415c9c
._______166 <retromac9x2@gmail.com>
2026-05-12 19:17:19 +0100

    fix nullpointerexception

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "io.github.dot166.flux"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = versionCode.toString()
     }
 
