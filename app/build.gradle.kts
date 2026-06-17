fab27a477ad0bbad6d1bc9e758ea95f885763012
._______166 <retromac9x2@gmail.com>
2026-06-17 22:44:34 +0100

    release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "io.github.dot166.flux"
         minSdk = 31
         targetSdk = 36
-        versionCode = 12
+        versionCode = 13
         versionName = versionCode.toString()
     }
 
